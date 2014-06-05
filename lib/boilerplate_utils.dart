db0380cca99f0b6313eb39e2f11cc536b0e660d9
Olivier Chafik <olivier.chafik@gmail.com>
2014-06-05 08:27:26 +0100

    split more files

---
+++ lib/boilerplate_utils.dart
@@ -0,0 +1,28 @@
+part of boilerplate.explicit;
+
+class BoilerplateUtils {
+
+  /// Equality object used to compare fields.
+  static const _equality = const DeepCollectionEquality();
+  
+  static int computeHashCode(Map<String, dynamic> fields) {
+    // Order of values matter: sort field names in case the map is unordered.
+     // TODO(ochafik): Have _TypeReflector sort its field names and always
+     //     assume "good" order (manual overrides of `get fields` would
+     //     typically use map literals, which are ordered maps).
+     List<String> names = new List.from(fields.keys)..sort();
+     return _equality.hash(names.map((name) => _equality.hash(fields[name])));
+  }
+  
+  /// Tests runtime types and same field values.
+  static bool equal(a, b) {
+      return (a.runtimeType == b.runtimeType)
+          && _equality.equals(a.fields, b.fields);
+  }
+  
+  static String makeString(String className, Map<String, dynamic> fields) {
+    // Getting fields might be very costly: store it to a local var.
+    var body = fields.keys.map((n) => "$n: ${fields[n].toString()}").join(', ');
+    return "$className { $body }";
+  }
+}
--- lib/explicit_boilerplate.dart
+++ lib/explicit_boilerplate.dart
@@ -2,6 +2,8 @@ library boilerplate.explicit;
 
 import 'package:collection/equality.dart';
 
+part 'boilerplate_utils.dart';
+
 /**
  * Mixin that implements hashCode, operator== and toString based on public
  * instance fields (explicit alternative to `Boilerplate` not using mirrors).
@@ -59,31 +61,4 @@ abstract class ExplicitBoilerplate {
 
   /// Basic toString implementation suitable mostly for debug purposes.
   String toString() => BoilerplateUtils.makeString(className, fields);
-}
-
-class BoilerplateUtils {
-
-  /// Equality object used to compare fields.
-  static const _equality = const DeepCollectionEquality();
-  
-  static int computeHashCode(Map<String, dynamic> fields) {
-    // Order of values matter: sort field names in case the map is unordered.
-     // TODO(ochafik): Have _TypeReflector sort its field names and always
-     //     assume "good" order (manual overrides of `get fields` would
-     //     typically use map literals, which are ordered maps).
-     List<String> names = new List.from(fields.keys)..sort();
-     return _equality.hash(names.map((name) => _equality.hash(fields[name])));
-  }
-  
-  /// Tests runtime types and same field values.
-  static bool equal(a, b) {
-      return (a.runtimeType == b.runtimeType)
-          && _equality.equals(a.fields, b.fields);
-  }
-  
-  static String makeString(String className, Map<String, dynamic> fields) {
-    // Getting fields might be very costly: store it to a local var.
-    var body = fields.keys.map((n) => "$n: ${fields[n].toString()}").join(', ');
-    return "$className { $body }";
-  }
 }
